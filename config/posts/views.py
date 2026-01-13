8c104703cc5cae7d67c31d0cef8f351106701eeb
黒川 透夜 <kurokawatouya@kurokawanoiMac.local>
2026-01-14 00:18:58 +0900

    Render posts list using Django templates

--- config/posts/views.py
+++ config/posts/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
-def index(request) :
-    return HttpResponse("Hello Django! Posts app is working.")
+def index(request):
+    posts = [
+        {"title": "最初の投稿"},
+        {"title": "Django楽しい"},
+        {"title": "SSR理解できた"},
+    ]
+
+    return render(request, "posts/index.html",{
+        "title": "Posts",
+        "massage": "Django テンプレート最高",
+        "posts" : posts,
+    })
