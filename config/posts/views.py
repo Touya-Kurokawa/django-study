6ef149049b7859008bc067ea5fd9637eadfc49d9
黒川 透夜 <kurokawatouya@kurokawanoiMac.local>
2026-01-20 21:13:28 +0900

    チャットフォームとPOST処理を追加

--- config/posts/views.py
+++ config/posts/views.py
@@ -8,8 +8,18 @@ def index(request):
         {"title": "SSR理解できた"},
     ]
 
+    user_message = ""
+    bot_message = ""
+
+    if request.method == "POST":
+        user_message = request.POST.get("message","") .strip()
+
+        bot_message = f"受け取ったよ : {user_message}"
+
     return render(request, "posts/index.html",{
         "title": "Posts",
-        "massage": "Django テンプレート最高",
+        "message": "Django テンプレート最高",
         "posts" : posts,
+        "user_message": user_message,
+        "bot_message": bot_message,
     })
